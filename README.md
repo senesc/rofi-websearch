32da204318e75f5e6d56e06afd8f8ed51ba16f40
Matteo Sambruna <29207130+senesc@users.noreply.github.com>
2020-12-23 20:31:12 +0100

    Added firefox and github support

--- README.md
+++ README.md
@@ -4,22 +4,25 @@ Rofi script for web search, allows you to quickly choose which search engine and
 ## Supported browsers/search engines
 Supported browsers are
 * chromium (c)
-* vivaldi-stable (v)
-launching in a new window is done by using the respective uppercase letter.
+* firefox (f)
+* vivaldi-stable (v)<br/>
+launching in a new window can be done by using the respective uppercase letter.
 
 Supported search engines are
 * Google (g)
 * YouTube (y)
 * DuckDuckGo (d)
-* [Genius](genius.com) (G)
+* GitHub (gh)
+* [Genius](genius.com) (gn)
 
 ## Usage
-You can run this script with the command `rofi -show websearch -modi "websearch:/path/to/rofi-websearch.sh"`
+You can run this script with the command `rofi -show websearch -modi "websearch:/path/to/rofi-websearch.sh"`<br/>
 The syntax is:
 ```
 [browser][search engine] [keywords]
 [browser] [url]
 ```
+
 ### Examples
-`cy kittens` looks for kittens videos on Youtube using Chromium
-`C github.com` opens GitHub in a new Chromium window
+`cgh rofi` looks for rofi on GitHub using Chromium<br/>
+`C github.com` opens GitHub in a new Chromium window<br/>
--- rofi-websearch.sh
+++ rofi-websearch.sh
@@ -28,13 +28,13 @@ then
 	echo -en "\0prompt\x1fSearch\t"
 	echo -en "\0message\x1fUsage: [browser][search engine] [keyword]\n       [browser] [url]\t"
 	echo -en "c - chromium\t"
-	echo -en "C - chromium in new window\t"
 	echo -en "v - vivaldi\t"
-	echo -en "V - vivaldi in new window\t"
+	echo -en "f - firefox\t"
 	echo -en "[x]g - google\t"
 	echo -en "[x]d - duckduckgo\t"
 	echo -en "[x]y - youtube\t"
-	echo -en "[x]G - genius\t"
+	echo -en "[x]gh - github\t"
+	echo -en "[x]gn - genius\t"
 
 elif [ $ROFI_RETV -eq 2 ]
 then
@@ -44,16 +44,23 @@ then
 		x"C") cmd="chromium --new-window ";;
 		x"v") cmd="vivaldi-stable ";;
 		x"V") cmd="vivaldi-stable --new-window ";;
+		x"f") cmd="firefox ";;
+		x"F") cmd="firefox --new-window ";;
 	esac
-	c2=$(echo $@ | cut -c 2)
-	if [[ x"$c2" != x" " ]]
+	c2=$(echo $@ | cut -c "2 3")
+	secondWord=$(echo $@ | sed 's/^..[ ]*//')
+	if [[ x"$c2" == x" " ]]
 	then
-		query=$(rawurlencode "$(echo $@ | sed 's/^..[ ]*//')")
+		cmd+=$secondWord
+		runBackground $cmd
+	else
+		query=$(rawurlencode "$secondWord")
 		case x"$c2" in
-			x"g") cmd+="https://google.com/search?q=$query"; runBackground $cmd;;
-			x"d") cmd+="https://duckduckgo.com/?q=$query"; runBackground $cmd;;
-			x"y") cmd+="https://youtube.com/results?search_qyuery=$query"; runBackground $cmd;;
-			x"G") cmd+="https://genius.com/search?q=$query"; runBackground $cmd;;
+			x"g ") cmd+="https://google.com/search?q=$query"; runBackground $cmd;;
+			x"d ") cmd+="https://duckduckgo.com/?q=$query"; runBackground $cmd;;
+			x"y ") cmd+="https://youtube.com/results?search_qyuery=$query"; runBackground $cmd;;
+			x"gh") cmd+="https://github.com/search?q=$query"; runBackground $cmd;;
+			x"gn") cmd+="https://genius.com/search?q=$query"; runBackground $cmd;;
 		esac
 	fi
 fi
